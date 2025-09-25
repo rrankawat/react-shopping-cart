85013c6d89b91c75be50223f289aaece4366de26
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2025-09-26 00:01:51 +0200

    Header & Cart Count

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import ProductList from './components/ProductList'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-gray-100 p-6'>
-      <h1 className='text-3xl font-bold mb-6'>🛒 Product Catalog</h1>
+    <>
+      <Header />
+      <div className='min-h-screen bg-gray-100 p-6'>
+        <h1 className='text-3xl font-bold mb-6'>🛒 Product Catalog</h1>
 
-      <ProductList />
-    </div>
+        <ProductList />
+      </div>
+    </>
   )
 }
 
